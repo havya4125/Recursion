3d05408b6a45b970be718937e77e33f6d619026b
Veethahavya <veethahavya@Veethahavyas-MacBook-Air.local>
2026-07-17 16:04:06 +0530

    feat(recursion): implement combination sum I

---
+++ combinationSumI.js
@@ -0,0 +1,23 @@
+// Problem Statement: Given an array of distinct positive integers and a target, find all unique combinations where the chosen numbers sum to the target.
+// ex: candidates = [2,3,6,7], target = 7      ans: [[2,2,3],[7]]
+
+const combinationSumI = (index, arr, ds, target, output) => {
+    if (index === arr.length) {
+        if (target === 0) {
+            output.push([...ds]);
+        }
+        return;
+    }
+    if (target >= arr[index]) {
+        ds.push(arr[index]);
+        combinationSumI(index, arr, ds, target - arr[index], output);
+        ds.pop()
+    }
+    combinationSumI(index + 1, arr, ds, target, output);
+};
+
+const arr = [2, 3, 6, 7];
+const target = 7;
+const ds = [];
+const output = [];
+combinationSumI(0, arr, ds, target, output);
